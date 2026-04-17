8e08115c4bec6718cd114df2a8b448d09b212bf4
DBU PREMIUM <81396690+PRODUPERR@users.noreply.github.com>
2026-04-17 19:39:01 -0400

    fix

--- src/routes/anime/index.ts
+++ src/routes/anime/index.ts
@@ -1,10 +1,8 @@
 import { FastifyRequest, FastifyReply, FastifyInstance, RegisterOptions } from 'fastify';
 import { PROVIDERS_LIST } from '@consumet/extensions';
 
-import gogoanime from './gogoanime';
 import animepahe from './animepahe';
 import zoro from './zoro';
-import nineanime from './9anime';
 import animefox from './animefox';
 import anify from './anify';
 import crunchyroll from './crunchyroll';
@@ -14,10 +12,8 @@ import anix from './anix';
 import animekai from './animekai';
 
 const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
-  await fastify.register(gogoanime, { prefix: '/gogoanime' });
   await fastify.register(animepahe, { prefix: '/animepahe' });
   await fastify.register(zoro, { prefix: '/zoro' });
-  await fastify.register(nineanime, { prefix: '/9anime' });
   await fastify.register(animefox, { prefix: '/animefox' });
   await fastify.register(anify, { prefix: '/anify' });
   await fastify.register(crunchyroll, { prefix: '/crunchyroll' });
